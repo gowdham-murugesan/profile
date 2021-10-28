a2c78f5785e24adacfb4d7ea0b02bf5a04ac9e0f
Gowdham M <gowdhammurugesh24@gmail.com>
2021-10-28 23:26:02 +0530

    Update phpmailer.php

--- phpmailer.php
+++ phpmailer.php
@@ -21,7 +21,7 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
-//     $mail->isSMTP();                                            //Send using SMTP
+    $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'counseling.gowdham@gmail.com';                     //SMTP username
