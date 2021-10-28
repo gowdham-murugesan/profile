258becaed067a07df8c4da96b72473ce67ddcf81
Gowdham M <gowdhammurugesh24@gmail.com>
2021-10-28 22:45:11 +0530

    Add files via upload

--- Homepage.js
+++ Homepage.js
@@ -120,3 +120,25 @@ if(screen.width < 600) {
   $(window).on('load', function () {
     $('#loading').fadeOut();
   });
+
+  //Scroll to Top
+
+  //Get the button
+    var mybutton = document.getElementById("movetop");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
---
+++ phpmailer.php
@@ -0,0 +1,56 @@
+<?php
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
+
+$html = `$name <br> $email <br> $message`;
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'counseling.gowdham@gmail.com';                     //SMTP username
+    $mail->Password   = 'Asdf@2000';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('counseling.gowdham@gmail.com', 'Gowdham M');
+    $mail->addAddress('gowdhammurugesh24@gmail.com', 'Gowdham M');     //Add a recipient
+    // $mail->addAddress($email, $name);             //Name is optional
+    // $mail->addReplyTo('gowdhammurugesh24@gmail.com', 'Admin');
+    // $mail->addCC('gowdhammurugesh24@gmail.com');
+    // $mail->addBCC('gowdhammurugesh24@gmail.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Profile - Mail';
+    $mail->Body    = $html;
+    $mail->AltBody = 'New user sent mail';
+
+    $mail->send();
+    echo 'Message has been sent';
+    header("location:Homepage.html");
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    header("location:Homepage.html");
+}
