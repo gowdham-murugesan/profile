a47f1757f5175ca352e15de74ad9adb9dc97cfb0
Gowdham M <gowdhammurugesh24@gmail.com>
2021-10-29 17:18:40 +0530

    Update phpmailer.php

--- phpmailer.php
+++ phpmailer.php
@@ -51,7 +51,7 @@ try {
     echo 'Message has been sent';
     echo "You will be redirected to main page in 3 seconds..."
 //     echo $html;
-    header( "refresh:3;url=index.html" );
+    header( "refresh:3;url=/" );
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     echo "You will be redirected to main page in 3 seconds..."
