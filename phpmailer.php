ac3ec24c1dd3f73bd4832f5953430a1e3099839e
Gowdham M <gowdhammurugesh24@gmail.com>
2021-11-03 09:02:34 +0530

    Update phpmailer.php

--- phpmailer.php
+++ phpmailer.php
@@ -1,55 +1,3 @@
-<?php
-$name = $_GET['name'];
-$email = $_GET['email'];
-$message = $_GET['message'];
-$html = ''.$name.' <br> '.$email.' <br> '.$message.'';
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'counseling.gowdham@gmail.com';                     //SMTP username
-//     $mail->Password   = 'Asdf@2000';                               //SMTP password
-    $mail->Password   = 'ibewtxlhgevaonvy';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('counseling.gowdham@gmail.com', 'Gowdham M');
-    $mail->addAddress('gowdhammurugesh24@gmail.com', 'Gowdham M');     //Add a recipient
-    // $mail->addAddress($email, $name);             //Name is optional
-    // $mail->addReplyTo('gowdhammurugesh24@gmail.com', 'Admin');
-    // $mail->addCC('gowdhammurugesh24@gmail.com');
-    // $mail->addBCC('gowdhammurugesh24@gmail.com');
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Profile - Mail';
-    $mail->Body    = $html;
-    $mail->AltBody = 'New user sent mail';
-    $mail->send();
-    // echo 'Message has been sent <br> You will be redirected to main page in 3 seconds...';
-//     echo $html;
-    header( "refresh:3;url=index.html" );
-} catch (Exception $e) {
-    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo} <br> You will be redirected to main page in 3 seconds...";
-//     echo $html;
-    header( "refresh:3;url=index.html" );
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -207,9 +155,9 @@ try {
 
         .success-message__icon {
             width: 275px;
-            margin-top: 130px;
             margin-left: 50%;
             transform: translateX(-50%);
+            margin-bottom: -130px;
         }
 
         @media only screen and (max-width: 480px) {
@@ -226,9 +174,18 @@ try {
 <body>
     <div class="loader abs tl"><!-- Loading --></div>
 	<div class="container abs">
-        <img src="https://cdn-user-icons.flaticon.com/56815/56815977/1635872009988.svg?token=exp=1635872928~hmac=64b1a1ce36cf1d5eee0f0f65af0798ca" alt="tick" class="success-message__icon" style="margin-bottom: -30px;">
-    	<h1>Mail Successfully Sent...!!</h1> 
-        <h3>You will be redirected to home page in 3 seconds...</h3>     
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 408.576 408.576" style="enable-background:new 0 0 512 512" xml:space="preserve" class="success-message__icon">
+            <circle r="204.288" cx="204.288" cy="204.288" fill="#57bc76" shape="circle" transform="matrix(0.63,0,0,0.63,75.58655792236328,75.58655792236328)"/>
+            <g transform="matrix(0.7,0,0,0.7,61.28639831542969,61.28639831542969)">
+                <g xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M204.288,0C91.648,0,0,91.648,0,204.288s91.648,204.288,204.288,204.288s204.288-91.648,204.288-204.288    S316.928,0,204.288,0z M318.464,150.528l-130.56,129.536c-7.68,7.68-19.968,8.192-28.16,0.512L90.624,217.6    c-8.192-7.68-8.704-20.48-1.536-28.672c7.68-8.192,20.48-8.704,28.672-1.024l54.784,50.176L289.28,121.344    c8.192-8.192,20.992-8.192,29.184,0C326.656,129.536,326.656,142.336,318.464,150.528z" fill="#ffffff" data-original="#000000" style="" class=""/>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        <h1>Mail Successfully Sent...!!</h1> 
+        <h3>You will be redirected to home page in 3 seconds...</h3>
     </div>
 </body>
 </html>
